78c7ea9c9f34f31ff2bfcb0b20d7323b7cff07f9
HYNNER <ogaming@seznam.cz>
2011-08-18 17:56:00 +0200

    initial commit

---
+++ DemoApp.hpp
@@ -0,0 +1,29 @@
+//|||||||||||||||||||||||||||||||||||||||||||||||
+#include "stdafx.h"
+#ifndef OGRE_DEMO_HPP
+#define OGRE_DEMO_HPP
+
+//|||||||||||||||||||||||||||||||||||||||||||||||
+
+#include "AdvancedOgreFramework.hpp"
+#include "AppStateManager.hpp"
+
+//|||||||||||||||||||||||||||||||||||||||||||||||
+
+class DemoApp
+{
+public:
+	DemoApp();
+	~DemoApp();
+
+	void startDemo();
+
+private:
+	AppStateManager*	m_pAppStateManager;
+};
+
+//|||||||||||||||||||||||||||||||||||||||||||||||
+
+#endif
+
+//|||||||||||||||||||||||||||||||||||||||||||||||
---
+++ stdafx.h
@@ -0,0 +1,22 @@
+#include <OgreCamera.h>
+#include <OgreEntity.h>
+#include <OgreLogManager.h>
+#include <OgreOverlay.h>
+#include <OgreOverlayElement.h>
+#include <OgreOverlayManager.h>
+#include <OgreRoot.h>
+#include <OgreViewport.h>
+#include <OgreSceneManager.h>
+#include <OgreRenderWindow.h>
+#include <OgreConfigFile.h>
+
+#include <OISEvents.h>
+#include <OISInputManager.h>
+#include <OISKeyboard.h>
+#include <OISMouse.h>
+
+#include <SdkTrays.h>
+#include <Ogre.h>
+#include <Terrain/OgreTerrain.h>
+#include <Terrain/OgreTerrainGroup.h>
+#include <Terrain/OgreTerrainMaterialGeneratorA.h>
